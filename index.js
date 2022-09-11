3e0c8e41e3e90762b1e94abac032043d201333e8
Halyna Tesliuk <halyna_tesliuk@epam.com>
2022-09-11 21:32:11 +0100

    added js template for weather-forecast

--- index.js
+++ index.js
@@ -27,6 +27,35 @@ function changeDate() {
     } ${date.getHours()}:${minutes}`;
 }
 
+function displayForecast(){
+    let forecastElement = document.querySelector("#forecast");
+    let days = ["Thu", "Fri", "Sat", "Sun"];
+
+    let forecastHTML = `<div class="row">`;
+    days.forEach(function (day) {
+        forecastHTML =
+            forecastHTML +
+            `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+    });
+
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+    console.log(forecastHTML);
+}
+
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(getWeatherData);
 
@@ -52,6 +81,8 @@ function getWeatherData(response) {
     );
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+
+displayForecast();
 
 function search(event) {
     event.preventDefault();
